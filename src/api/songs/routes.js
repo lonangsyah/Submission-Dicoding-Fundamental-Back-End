701c575bcca8e98d1c0ecfe92d5d616a665e329b
Syahrani Lonang <40293450+lonangsyah@users.noreply.github.com>
2022-04-22 21:14:09 +0800

    Add files via upload

---
+++ src/api/songs/routes.js
@@ -0,0 +1,31 @@
+const routes = (handler) => [
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: handler.getSongsHandler,
+  },
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: handler.postSongHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs/{id}',
+    handler: handler.getSongByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{id}',
+    handler: handler.putSongByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{id}',
+    handler: handler.deleteSongByIdHandler,
+  },
+
+];
+
+module.exports = routes;
+
